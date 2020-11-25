cbf2e15c6176b642c4288702dc73fdfc887c0dd8
hbarbosams <74909637+hbarbosams@users.noreply.github.com>
2020-11-25 17:18:55 -0300

    Freelancer atualizado

---
+++ Controllers/FreelancerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]  
+    public class FreelancerController : Controller
+    {
+        [Route("api/[controller]/Read")]
+        [HttpGet]        
+         public IActionResult Read(int id)
+
+        {
+            var resultado = new Freelancer();
+                using (var data = new FreelancerData())
+                resultado = data.Read(id);
+                return  Ok(resultado);
+        }
+
+        [Route("api/[controller]/Create")]
+        [HttpPost]
+        public IActionResult Create([FromBody] Freelancer freelancer)
+        {
+            string nome = freelancer.Nome;   //<input name = "nome"
+            string login = freelancer.Login; //<input login = "login"
+            string senha = freelancer.Senha; //<input senha = "senha"
+            string telefone = freelancer.Telefone; //<input telefone = "telefone"
+            string email = freelancer.Email; //<input email = "email"
+            string cpf = freelancer.Cpf;
+            string ra = freelancer.Ra;
+            string experiencia = freelancer.Experiencia;
+
+            using (var data = new FreelancerData())
+                data.Create(freelancer);
+            return Ok(freelancer);
+        }
+    }
+}
---
+++ Data/FreelancerData.cs
@@ -0,0 +1,151 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class FreelancerData : Data
+    {
+        public void Create(Freelancer freelancer)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connectionDB;
+
+            // Comando que sera escrito no banco de dados
+            cmd.CommandText = @"exec AddFreela @nome, @login, @senha, @email, @telefone, @qtdProjetos,
+                                               @mediaNota, @status, @cpf, @ra, @experiencia";
+
+            cmd.Parameters.AddWithValue("@nome", freelancer.Nome);
+            cmd.Parameters.AddWithValue("@login", freelancer.Login);
+            cmd.Parameters.AddWithValue("@senha", freelancer.Senha);
+            cmd.Parameters.AddWithValue("@status", freelancer.Status);
+            cmd.Parameters.AddWithValue("@telefone", freelancer.Telefone);
+            cmd.Parameters.AddWithValue("@qtdprojetos", freelancer.QtdProjetos);
+            cmd.Parameters.AddWithValue("@medianota", freelancer.MediaNota);
+            cmd.Parameters.AddWithValue("@email", freelancer.Email);
+
+            // Colocando os dados recebidos pelo objeto cliente na string sql
+            cmd.Parameters.AddWithValue("@cpf", freelancer.Cpf);
+            cmd.Parameters.AddWithValue("@ra", freelancer.Ra);
+            cmd.Parameters.AddWithValue("@experiencia", freelancer.Experiencia);
+
+            // Execução da string qld no banco
+            cmd.ExecuteNonQuery();
+
+            
+        }
+ 
+        public Freelancer Read(string login)
+        {
+            Freelancer freelancer = null;
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connectionDB;
+
+            cmd.CommandText = @"SELECT * FROM  Pessoa, Freelancer WHERE Login = @login AND pessoa.id = Freelancer.freelancer_id";
+
+            cmd.Parameters.AddWithValue("@login", login);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                freelancer = new Freelancer
+                {
+                    // Criando objeto pessoa que existe no banco
+                    Id = (int)reader["Id"],
+                    Nome = (string)reader["Nome"],
+                    Cpf = (string)reader["Cpf"],
+                    Login = (string)reader["Login"],
+                    Senha = (string)reader["Senha"],
+                    Status = (int)reader["Status"],
+                    Telefone = (string)reader["Telefone"],
+                    QtdProjetos = (int)reader["QtdProjetos"],
+                    MediaNota = (decimal)reader["MediaNota"],
+                    Email = (string)reader["Email"],
+                    Ra = (string)reader["Ra"],
+                    Experiencia = (string)reader["Experiencia"]
+                };
+            }
+            return freelancer;
+        }
+
+        public Freelancer Read(int id)
+        {
+            Freelancer freelancer = null;
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connectionDB;
+
+            cmd.CommandText = @"SELECT * FROM Pessoa, freelancer WHERE Id = @id AND Pessoa.id = freelancer.freelancer_id";
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            // verifica se apos a consulta retornou um registro
+            if (reader.Read())
+            {
+                // Instancia o objeto cliente outra forma de ler
+                freelancer = new Freelancer
+                {
+                    Id = (int)reader["Id"],
+                    Nome = (string)reader["Nome"],
+                    Cpf = (string)reader["Cpf"],
+                    Login = (string)reader["Login"],
+                    Senha = (string)reader["Senha"],
+                    Status = (int)reader["Status"],
+                    Telefone = (string)reader["Telefone"],
+                    QtdProjetos = (int)reader["QtdProjetos"],
+                    MediaNota = (decimal)reader["MediaNota"],
+                    Email = (string)reader["Email"],
+                    Ra = (string)reader["Ra"],
+                    Experiencia = (string)reader["Experiencia"]
+                };
+            }
+            return freelancer;
+        }
+
+        public void Update(Freelancer freelancer)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = connectionDB;
+
+            cmd.CommandText = @"exec UpdateFreela @id, @nome, @login, @senha, @email, @telefone, 
+                                                  @qtdProjetos, @mediaNota, @status, @ra, @experiencia";
+
+            cmd.Parameters.AddWithValue("@id", freelancer.Id);
+            cmd.Parameters.AddWithValue("@nome", freelancer.Nome);
+            cmd.Parameters.AddWithValue("@login", freelancer.Login);
+            cmd.Parameters.AddWithValue("@senha", freelancer.Senha);
+            cmd.Parameters.AddWithValue("@status", freelancer.Status);
+            cmd.Parameters.AddWithValue("@telefone", freelancer.Telefone);
+            cmd.Parameters.AddWithValue("@qtdprojetos", freelancer.QtdProjetos);
+            cmd.Parameters.AddWithValue("@medianota", freelancer.MediaNota);
+            cmd.Parameters.AddWithValue("@email", freelancer.Email);
+
+            cmd.Parameters.AddWithValue("@ra", freelancer.Ra);
+            cmd.Parameters.AddWithValue("@experiencia", freelancer.Experiencia);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Delete(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = connectionDB;
+
+            cmd.CommandText = @"DELETE FROM Freelancer WHERE Id = @id";
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
